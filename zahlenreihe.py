4b3be68b1b93423d24cf744879c045fc6f107676
aguetler25 <aguetler25@icapital.at>
2018-04-18 11:57:39 +0200

    zahlenreihe fertig

--- zahlenreihe.py
+++ zahlenreihe.py
@@ -11,14 +11,19 @@ for i in range(1, n+1):
         print(i)
 
 # alle geraden Zahlen von 1..n ausgeben
+print("")
 for i in range(1, n+1):
     if (i % 2 == 0):
         print(i, end=" ")
 
 # alle ungeraden Zahlen von 1..n ausgeben
+print("")
 for i in range(1, n+1):
     if (i % 2 != 0):
         print(i, end=" ")
 
 # alle Zahlen von 1..n ausgeben, die durch 9 teilbar sind
-
+print("")
+for i in range(1, n+1):
+    if (i % 9 == 0):
+        print(i, end=" ")
