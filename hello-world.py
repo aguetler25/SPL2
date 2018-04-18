91ef7f7e6a5a8b3fc3acccc1923c51f27b291170
aguetler25 <aguetler25@icapital.at>
2018-04-18 09:33:54 +0200

    erstes python programm

---
+++ hello-world.py
@@ -0,0 +1,6 @@
+print ("Hallo, ich bin ein Computer.")
+name = input("Wie ist dein Name? ")
+print ("Schön dich zu treffen ", name, ".", sep="")
+print ("Dein Name ist ", len(name), "Zeichen lang.")
+alter = input("Wie alt bist du? ")
+print ("Nächstes Jahr wirst du", int(alter)+1, "Jahre alt sein.")
