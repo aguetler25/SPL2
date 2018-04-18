a119dd813daf5c751190f5b49670775e455a145c
aguetler25 <aguetler25@icapital.at>
2018-04-18 11:30:38 +0200

    zahlenreihe.py angelegt

---
+++ zahlenreihe.py
@@ -0,0 +1,17 @@
+# zahlenreihe.py
+
+n = input("Bitte n eingeben: ")
+n = int(n)
+
+for i in range(1, n+1):
+    if (i < n):
+        print(i, end=" < ")
+    else:
+        print(i)
+
+# alle geraden Zahlen von 1..n ausgeben
+
+# alle ungeraden Zahlen von 1..n ausgeben
+
+# alle Zahlen von 1..n ausgeben, die durch 9 teilbar sind
+
